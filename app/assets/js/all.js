4c1da07c0149c17aa1bef7e8a144ac5713695b51
Shang <shang801104@gmail.com>
2022-09-10 15:50:41 +0800

    第1次存檔

---
+++ app/assets/js/all.js
@@ -0,0 +1,19 @@
+// Swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
