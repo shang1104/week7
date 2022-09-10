bab9318dd242881883a56c2c4549984b97ff66f0
Shang <shang801104@gmail.com>
2022-09-10 15:53:00 +0800

    Update 2022-09-10T07:52:54.987Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,19 +1,20 @@
+"use strict";
+
 // Swiper
-const swiper = new Swiper(".swiper", {
+var swiper = new Swiper(".swiper", {
   // Optional parameters
   effect: "fade",
   fadeEffect: {
-    crossFade: true,
+    crossFade: true
   },
-
   // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination"
   },
-
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    prevEl: ".swiper-button-prev"
+  }
 });
+//# sourceMappingURL=all.js.map
