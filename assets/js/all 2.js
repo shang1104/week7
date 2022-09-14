57350eacd73a51ea300c44decd870a01b75168d7
Shang <shang801104@gmail.com>
2022-09-14 23:41:27 +0800

    Update 2022-09-14T15:41:24.567Z

---
+++ assets/js/all 2.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Swiper
+var swiper = new Swiper(".swiper", {
+  // Optional parameters
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+//# sourceMappingURL=all.js.map
